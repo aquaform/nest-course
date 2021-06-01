a527c65bd386516ca48f3598797cd060db19bb9b
Kanifor <vladimir.panasenya@gmail.com>
2021-06-01 20:47:56 +0500

    work

--- src/users/dto/create-user-dto.ts
+++ src/users/dto/create-user-dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import {IsEmail, IsString, Length} from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({example: 'aquaform@yandex.ru', description: 'Электронная почта'})
+  @IsString({message: 'Должно быть строкой'})
+  @IsEmail({}, {message: 'Некорректный емайл'})
   readonly email: string;
   @ApiProperty({example: 'SexGod123', description: 'Пароль'})
+  @IsString({message: 'Должно быть строкой'})
+  @Length(4, 16, {message: 'Не меньше 4 и не больше 16'})
   readonly password: string;
 }
